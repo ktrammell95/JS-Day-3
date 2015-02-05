4ec490495bc63e98fc2cc66db06889113548888c
Katherine Trammell <ktrammell@gmail.com>
2015-02-05 15:14:34 -0500

    Started answering exercise questions

--- excercises.js
+++ excercises.js
@@ -3,11 +3,14 @@
  */
 
 // ---------------------
-// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// Define a function max() that takes two numbers as arguments and returns the largest of them. 
+//Use the if-then-else construct available in JavaScript.
 // ---------------------
+var values = [25, 50]
 
-function max(){
-    //...
+var max = values.filter(function(item){
+    if 
+    return 
 }
 
 // ---------------------
@@ -27,16 +30,21 @@ function isVowel(char){
 }
 
 // ---------------------
-// Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
+// Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. 
+//For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
 // ---------------------
 
-function sum(){
-    //...
-}
+var numbers = [10, 20, 30, 40];
 
-function multiply(){
-    //...
-}
+var sum = numbers.reduce(function(memo, item) {
+  return memo + item;
+});
+
+var numbers = [1, 2, 3, 4];
+
+var multiply = numbers.reduce(function(memo, item) {
+  return memo * item;
+});
 
 // ---------------------
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
